a5f77d059887590c1327cf10372724a9a0e33400
Alexandro <alexlmangabeira@gmail.com>
2022-02-01 18:48:02 -0300

    função calculaImposto

--- funcoes/esencial/funcoes.js
+++ funcoes/esencial/funcoes.js
@@ -23,7 +23,13 @@ console.log(capitaliza);
 // Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
 // (30, Alimentação) => 0
 // (10, Bebida) => 1
-function calculaImposto() { }
+function calculaImposto(preco, categoria) {
+    return categoria.includes("Alimentação") ? 0 : ((preco * 10) /100);
+}
+const preco = 10;
+const categoria = 'Bebida';
+const calImposto = calculaImposto(preco, categoria);
+console.log(calImposto);
 
 // Escreva uma função que recebe um preço original, uma categoria de produto e um cupom de desconto e calcula o preço com desconto. Se a categoria for Alimentação e o cupom for NULABSSA, deve ser feito um desconto de 50%. Caso contrário, não há nenhum desconto.
 // (30, Alimentação, NULABSSA) => 15
