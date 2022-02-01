c498ba03c628ea016e1190d4fff38ac4c5e747e5
Alexandro <alexlmangabeira@gmail.com>
2022-02-01 18:05:49 -0300

    função saudar

--- funcoes/esencial/funcoes.js
+++ funcoes/esencial/funcoes.js
@@ -1,5 +1,9 @@
 // Escreva uma função que receba um nome e retorne uma saudação para este nome: Tiago -> Olá, Tiago
-function saudar() { }
+function saudar( nome ) {
+    return `Olá, ${nome}`;
+}
+const nome = 'Alexandro';
+saudar(nome);
 // Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
 function extrairPrimeiroNome() { }
 
