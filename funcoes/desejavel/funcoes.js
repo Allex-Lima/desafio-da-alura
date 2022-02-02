9be5442a98db04422c94874934364028d69b3a4a
Alexandro <alexlmangabeira@gmail.com>
2022-02-02 12:11:47 -0300

    funções desejavel - truncar palavra

--- funcoes/desejavel/funcoes.js
+++ funcoes/desejavel/funcoes.js
@@ -2,10 +2,19 @@
 // o valor default do comprimento máximo deve ser 5:
 // (teste, 10) -> teste
 // (fulano, 4) -> fula...
-function truncar() { }
+function truncar(palavra, comprimentoMax = 5) {
+    if (palavra.length <= comprimentoMax) {
+        return `${palavra}`;
+    }
+    return `${palavra.slice(0, comprimentoMax)}...`;
+}
+const palavra = 'teste';
+const comprimentoMax = 10;
+const truncarPalavra = truncar(palavra, comprimentoMax);
+console.log(truncarPalavra);
 
 // Escreva uma função que valida se o texto informado está preenchido e retorna o texto sem espaços antes ou depois.
 // "" -> undefined
 // "   " -> undefined
 // "      Maria " -> "Maria"
-function validaTextoPreenchido() { }
+function validaTextoPreenchido() {}
